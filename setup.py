07655dffa6d08eb937a2bc16f09a8444bc63b78b
root <root@vmi1531542.contaboserver.net>
2024-02-26 18:34:11 -0500

    Initial commit

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'yavuz_unique==0.1.0', # Gerekli bağımlılıkları buraya ekleyin
+        'yavuz_unique==0.1.0',  # Gerekli bağımlılıkları buraya ekleyin
     ],
     entry_points={
         'console_scripts': [
             'my-notebook-app=my_notebook_app.main:main',
         ],
     },
+    url='https://github.com/hw010101/my_notebook_app',  # GitHub deposunun URL'sini ekleyin
 )
