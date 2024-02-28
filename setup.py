47cc4934f6a86259a121d51c3127e6bc012aaf3f
root <root@vmi1531542.contaboserver.net>
2024-02-28 12:55:11 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='my_notebook_app',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
-        'yavuz_unique==0.1.0',  # Gerekli bağımlılıkları buraya ekleyin
+        'yavuz_unique==0.1.6',  # Gerekli bağımlılıkları buraya ekleyin
     ],
     entry_points={
         'console_scripts': [
             'my-notebook-app=my_notebook_app.main:main',
         ],
     },
-    url='https://github.com/hw010101/my_notebook_app',  # GitHub deposunun URL'sini ekleyin
+    author='lilzey',
+    author_email='lilzey0101@gmail.com',
+    description='notebook',
+    url='https://github.com/hw010101/my_notebook_app',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+    project_urls={
+        'Source': 'https://github.com/hw010101/my_notebook_app',
+    },
 )
